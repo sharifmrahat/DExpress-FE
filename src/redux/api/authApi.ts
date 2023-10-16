cfc48fa9ad8f38d808b5bdaa9f1f38197a987987
sharifmrahat <sharifmrahat@gmail.com>
2023-10-16 20:37:34 +0600

    login signup reducer is added

--- src/redux/api/authApi.ts
+++ src/redux/api/authApi.ts
@@ -12,6 +12,14 @@ export const authApi = baseApi.injectEndpoints({
       }),
       invalidatesTags: [tagTypes.user],
     }),
+    userSignup: build.mutation({
+      query: (signupData) => ({
+        url: `${AUTH_URL}/signup`,
+        method: "POST",
+        data: signupData,
+      }),
+      invalidatesTags: [tagTypes.user],
+    }),
   }),
 });
 
