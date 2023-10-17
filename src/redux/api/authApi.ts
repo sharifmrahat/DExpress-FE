301fcf091fa447f49042523aed6fb236d119ebcf
sharifmrahat <sharifmrahat@gmail.com>
2023-10-17 22:42:09 +0600

    auth api is updated

--- src/redux/api/authApi.ts
+++ src/redux/api/authApi.ts
@@ -6,7 +6,7 @@ export const authApi = baseApi.injectEndpoints({
   endpoints: (build) => ({
     userLogin: build.mutation({
       query: (loginData) => ({
-        url: `${AUTH_URL}/signin`,
+        url: `${AUTH_URL}/login`,
         method: "POST",
         data: loginData,
       }),
