fd85de7b95270658540dccc8e50998e08335184f
sharifmrahat <sharifmrahat@gmail.com>
2024-05-05 18:30:01 +0600

    empty state design is added

--- src/components/common/DataNotFound.tsx
+++ src/components/common/DataNotFound.tsx
@@ -1,25 +1,35 @@
 "use client";
 
-import { Alert } from "@mantine/core";
-import { IconMoodSad } from "@tabler/icons-react";
+import { Paper, Text } from "@mantine/core";
+import { IconFileSad, IconMoodSad } from "@tabler/icons-react";
+import { Oswald } from "next/font/google";
+const oswald = Oswald({ style: "normal", weight: "400", subsets: ["latin"] });
 
 const DataNotFound = ({
-  variant = "light",
-  color = "#ff3f39",
   title = "Data Not Found!",
   description = "No data is found",
-  icon = <IconMoodSad />,
+  icon = <IconFileSad size={60} stroke={1} />,
+  bgColor = "background",
 }: {
-  variant?: string;
-  color?: string;
   title?: string;
   description?: string;
   icon?: JSX.Element;
+  bgColor?: string;
 }) => {
   return (
-    <Alert variant={variant} color={color} title={title} icon={icon}>
-      {description}
-    </Alert>
+    <Paper p="md" className={`w-full bg-${bgColor}`}>
+      <div className="flex flex-col justify-center items-center gap-3">
+        <div className="text-gray-400 w-fit mx-auto">{icon}</div>
+        <div className="flex flex-col justify-center items-center gap-1">
+          <Text className={`${oswald.className} text-lg lg:text-xl max-w-sm`}>
+            {title}
+          </Text>
+          <Text c="dimmed" className="text-sm lg:text-base max-w-sm">
+            {description}
+          </Text>
+        </div>
+      </div>
+    </Paper>
   );
 };
 
