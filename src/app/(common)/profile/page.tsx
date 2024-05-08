6fdab6a8e4e0e5da1b91d625f557ad8de8deeb2a
sharifmrahat <sharifmrahat@gmail.com>
2024-05-08 22:24:56 +0600

    profile page skeleton loader is added

--- src/app/(common)/profile/page.tsx
+++ src/app/(common)/profile/page.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @next/next/no-img-element */
 "use client";
+import DataNotFound from "@/components/common/DataNotFound";
+import SkeletonLoader from "@/components/common/SkeletonLoader";
 import Spinner from "@/components/common/Spinner";
 import {
   useUpdateProfileMutation,
@@ -14,6 +16,7 @@ import {
   Button,
   Group,
   LoadingOverlay,
+  Skeleton,
   Tabs,
   Text,
   TextInput,
@@ -29,7 +32,7 @@ const ProfilePage = () => {
   const [editable, setEditable] = useState(false);
   const [visible, { toggle }] = useDisclosure(false);
   const [activeTab, setActiveTab] = useState<string | null>("personal");
-  const { data, isLoading, refetch } = useUserProfileQuery({});
+  const { data, isLoading, isSuccess, refetch } = useUserProfileQuery({});
 
   const [updateProfile, { isLoading: loadingUpdate }] =
     useUpdateProfileMutation();
@@ -108,145 +111,180 @@ const ProfilePage = () => {
   return (
     <div className="p-4 lg:p-8">
       <Breadcrumbs className="hidden lg:flex">{routes}</Breadcrumbs>
-      <div className="mt-0 lg:mt-10 mx-0">
-        <div className="flex flex-col lg:flex-row gap-5 justify-center lg:justify-start items-center lg:items-start">
-          <Avatar
-            src={profile?.imageUrl}
-            className="border-2 border-primary cursor-pointer"
-            alt={profile?.name}
-            color="#ff3f39"
-            size="xl"
-          >
-            {profile?.name?.charAt(0)?.toUpperCase()}
-          </Avatar>
-          <div className="flex flex-col gap-4 justify-center items-center lg:justify-start lg:items-start">
-            <Text className="text-2xl lg:text-3xl font-semibold">
-              {profile?.name}
-            </Text>
-            <Badge variant="light" color="#ff3f39" size="md">
-              {profile?.role}
-            </Badge>
-            {/* <Badge color={profile?.isVerified ? "teal" : "dark"}>
+      {isSuccess && profile ? (
+        <>
+          <div className="mt-0 lg:mt-10 mx-0">
+            <div className="flex flex-col lg:flex-row gap-5 justify-center lg:justify-start items-center lg:items-start">
+              <Avatar
+                src={profile?.imageUrl}
+                className="border-2 border-primary cursor-pointer"
+                alt={profile?.name}
+                color="#ff3f39"
+                size="xl"
+              >
+                {profile?.name?.charAt(0)?.toUpperCase()}
+              </Avatar>
+              <div className="flex flex-col gap-4 justify-center items-center lg:justify-start lg:items-start">
+                <Text className="text-2xl lg:text-3xl font-semibold">
+                  {profile?.name}
+                </Text>
+                <Badge variant="light" color="#ff3f39" size="md">
+                  {profile?.role}
+                </Badge>
+                {/* <Badge color={profile?.isVerified ? "teal" : "dark"}>
               {profile?.isVerified ? "Verified" : "Not Verified"}
             </Badge> */}
+              </div>
+            </div>
+          </div>
+
+          <div className="mt-10">
+            <Tabs
+              color="#ff3f39"
+              defaultValue="personal"
+              value={activeTab}
+              onChange={(val) => setActiveTab(val)}
+            >
+              <Tabs.List>
+                <Tabs.Tab
+                  value="personal"
+                  className={`hover:text-primary font-semibold hover:bg-transparent ${
+                    activeTab === "personal" && "text-primary"
+                  }`}
+                >
+                  Personal Info
+                </Tabs.Tab>
+                <Tabs.Tab
+                  value="settings"
+                  className={`hover:text-primary font-semibold hover:bg-transparent ${
+                    activeTab === "settings" && "text-primary"
+                  }`}
+                >
+                  Account Settings
+                </Tabs.Tab>
+              </Tabs.List>
+
+              <Tabs.Panel value="personal" className="p-4">
+                <Box pos="relative" className="w-full lg:w-1/3">
+                  <LoadingOverlay
+                    visible={loadingUpdate ?? visible}
+                    zIndex={1000}
+                    overlayProps={{ radius: "sm", blur: 0 }}
+                    loaderProps={{
+                      children: (
+                        <div className="w-fit mb-20">
+                          <Spinner />
+                        </div>
+                      ),
+                    }}
+                  />
+                  <form
+                    className="flex flex-col gap-3"
+                    onSubmit={form.onSubmit((values) =>
+                      submitUpdate({
+                        name: values?.name,
+                        imageUrl: values?.imageUrl ?? "",
+                        contactNo: values?.contactNo ?? "",
+                      })
+                    )}
+                  >
+                    <TextInput
+                      label="Name"
+                      placeholder="Your Name"
+                      key={form.key("name")}
+                      {...form.getInputProps("name")}
+                      disabled={!editable}
+                    />
+                    <TextInput
+                      label="Profile Picture"
+                      placeholder="Profile Picture"
+                      key={form.key("imageUrl")}
+                      {...form.getInputProps("imageUrl")}
+                      disabled={!editable}
+                    />
+                    <TextInput
+                      label="Email"
+                      placeholder="Your Email"
+                      key={form.key("email")}
+                      {...form.getInputProps("email")}
+                      disabled
+                    />
+                    <TextInput
+                      label="Contact No."
+                      placeholder="Contact No."
+                      key={form.key("contactNo")}
+                      {...form.getInputProps("contactNo")}
+                      disabled={!editable}
+                    />
+
+                    {!editable && (
+                      <Group className="mt-4 flex justify-end">
+                        <Button
+                          variant="light"
+                          color="#ff3f39"
+                          onClick={() => setEditable(true)}
+                        >
+                          Edit
+                        </Button>
+                      </Group>
+                    )}
+
+                    {editable && (
+                      <Group className="mt-4 flex justify-end">
+                        <Button
+                          variant="outline"
+                          color="gray"
+                          onClick={() => setEditable(false)}
+                        >
+                          Cancel
+                        </Button>
+                        <Button type="submit" color="#ff3f39">
+                          Update
+                        </Button>
+                      </Group>
+                    )}
+                  </form>
+                </Box>
+              </Tabs.Panel>
+
+              <Tabs.Panel value="settings">
+                <div className="p-4">Account Settings</div>
+              </Tabs.Panel>
+            </Tabs>
+          </div>
+        </>
+      ) : isLoading ? (
+        <div className="mt-0 lg:mt-10 mx-0">
+          <div className="flex flex-col lg:flex-row gap-5 justify-center lg:justify-start items-center px-2">
+            <div className="w-fit mx-auto lg:mx-2">
+              <Skeleton height={80} circle />
+            </div>
+            <Skeleton
+              height={60}
+              radius="md"
+              width="20%"
+              className="hidden lg:block"
+            />
+            <Skeleton
+              height={40}
+              radius="md"
+              width="50%"
+              className="block lg:hidden"
+            />
+          </div>
+
+          <div className="mt-20 px-2">
+            <Skeleton height={20} className="hidden lg:block" mb={20} />
+            <SkeletonLoader>
+              <div className="w-full h-[425px]"></div>
+            </SkeletonLoader>
           </div>
         </div>
-      </div>
-
-      <div className="mt-10">
-        <Tabs
-          color="#ff3f39"
-          defaultValue="personal"
-          value={activeTab}
-          onChange={(val) => setActiveTab(val)}
-        >
-          <Tabs.List>
-            <Tabs.Tab
-              value="personal"
-              className={`hover:text-primary font-semibold hover:bg-transparent ${
-                activeTab === "personal" && "text-primary"
-              }`}
-            >
-              Personal Info
-            </Tabs.Tab>
-            <Tabs.Tab
-              value="settings"
-              className={`hover:text-primary font-semibold hover:bg-transparent ${
-                activeTab === "settings" && "text-primary"
-              }`}
-            >
-              Account Settings
-            </Tabs.Tab>
-          </Tabs.List>
-
-          <Tabs.Panel value="personal" className="p-4">
-            <Box pos="relative" className="w-full lg:w-1/3">
-              <LoadingOverlay
-                visible={loadingUpdate ?? visible}
-                zIndex={1000}
-                overlayProps={{ radius: "sm", blur: 0 }}
-                loaderProps={{
-                  children: (
-                    <div className="w-fit mb-20">
-                      <Spinner />
-                    </div>
-                  ),
-                }}
-              />
-              <form
-                className="flex flex-col gap-3"
-                onSubmit={form.onSubmit((values) =>
-                  submitUpdate({
-                    name: values?.name,
-                    imageUrl: values?.imageUrl ?? "",
-                    contactNo: values?.contactNo ?? "",
-                  })
-                )}
-              >
-                <TextInput
-                  label="Name"
-                  placeholder="Your Name"
-                  key={form.key("name")}
-                  {...form.getInputProps("name")}
-                  disabled={!editable}
-                />
-                <TextInput
-                  label="Profile Picture"
-                  placeholder="Profile Picture"
-                  key={form.key("imageUrl")}
-                  {...form.getInputProps("imageUrl")}
-                  disabled={!editable}
-                />
-                <TextInput
-                  label="Email"
-                  placeholder="Your Email"
-                  key={form.key("email")}
-                  {...form.getInputProps("email")}
-                  disabled
-                />
-                <TextInput
-                  label="Contact No."
-                  placeholder="Contact No."
-                  key={form.key("contactNo")}
-                  {...form.getInputProps("contactNo")}
-                  disabled={!editable}
-                />
-
-                {!editable && (
-                  <Group className="mt-4 flex justify-end">
-                    <Button
-                      variant="light"
-                      color="#ff3f39"
-                      onClick={() => setEditable(true)}
-                    >
-                      Edit
-                    </Button>
-                  </Group>
-                )}
-
-                {editable && (
-                  <Group className="mt-4 flex justify-end">
-                    <Button
-                      variant="outline"
-                      color="gray"
-                      onClick={() => setEditable(false)}
-                    >
-                      Cancel
-                    </Button>
-                    <Button type="submit" color="#ff3f39">
-                      Update
-                    </Button>
-                  </Group>
-                )}
-              </form>
-            </Box>
-          </Tabs.Panel>
-
-          <Tabs.Panel value="settings">
-            <div className="p-4">Account Settings</div>
-          </Tabs.Panel>
-        </Tabs>
-      </div>
+      ) : (
+        <div className="flex justify-start items-start mt-0 lg:mt-10 mx-0">
+          <DataNotFound description="No profile data is found" />
+        </div>
+      )}
     </div>
   );
 };
